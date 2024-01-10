aa3a83a9050f8b79dbeb5517edd14ece632f52a0
Alexey <alexey.kostenyu@gmail.com>
2024-01-10 21:50:09 +0400

    Complete commit

---
+++ users/views.py
@@ -0,0 +1,13 @@
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from users.models import User
+from users.serializers import UserListSerializer, UserDetailSerializer
+
+
+class UserListView(ListAPIView):
+    serializer_class = UserListSerializer
+    queryset = User.objects.all()
+
+
+class UserDetailView(RetrieveAPIView):
+    serializer_class = UserDetailSerializer
+    queryset = User.objects.all()
